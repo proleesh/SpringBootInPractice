d23233a75ba332570e313ed63563aad05548a976
proleesh <merci726@yahoo.com>
2024-09-23 14:56:16 +0900

    update about validation

--- src/main/java/org/proleesh/SpringBootInPracticeApplication.java
+++ src/main/java/org/proleesh/SpringBootInPracticeApplication.java
@@ -1,13 +1,36 @@
 package org.proleesh;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.proleesh.model.Course;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Set;
+
+
 @SpringBootApplication
-public class SpringBootInPracticeApplication {
+public class SpringBootInPracticeApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootInPracticeApplication.class, args);
     }
 
+    private static Logger logger = LoggerFactory.getLogger(SpringBootInPracticeApplication.class);
+    @Override
+    public void run(String... args) throws Exception {
+        Course course = new Course();
+        course.setId(1);
+        course.setRating(4);
+
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        Set<ConstraintViolation<Course>> violations = validator.validate(course);
+
+        violations.forEach(courseConstraintViolation -> logger.error("A constraint violation has occurred. Violation details:[{}].", courseConstraintViolation));
+
+    }
 }
--- src/main/java/org/proleesh/model/Course.java
+++ src/main/java/org/proleesh/model/Course.java
@@ -1,4 +1,22 @@
 package org.proleesh.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
 public class Course {
+    private long id;
+    private String name;
+    private String category;
+
+    @Min(value = 1, message = "A course should have a minimum of 1 rating")
+    @Max(value = 5, message = "A course should have a maximum of 5 rating")
+    private int rating;
+
+    private String description;
+
 }
