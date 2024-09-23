02b8b70f8abd686fd7ad366a97edc0ca41223ca9
proleesh <merci726@yahoo.com>
2024-09-23 16:29:34 +0900

    update about CommandLineRunner and any

--- SpringBootInPractice2/src/main/java/org/proleesh/SpringBootInPractice2Application.java
+++ SpringBootInPractice2/src/main/java/org/proleesh/SpringBootInPractice2Application.java
@@ -5,6 +5,7 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootInPractice2Application implements CommandLineRunner {
@@ -13,6 +14,16 @@ public class SpringBootInPractice2Application implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootInPractice2Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            logger.info("CommandLineRunnere executed as a bean definition with " + args.length + " arguments");
+            for(int i = 0; i < args.length; ++i){
+                logger.info("Argument: " + args[i]);
+            }
+        };
     }
 
     @Override
