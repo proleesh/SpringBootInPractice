24702123f5e3263e2675c9a5624adaee9a4b802c
proleesh <merci726@yahoo.com>
2024-09-24 20:43:32 +0900

    update about Customized Repository

--- SpringBootInPractice3/src/main/java/org/proleesh/springbootinpractice3/repository/CustomizedCourseRepository.java
+++
@@ -1,8 +0,0 @@
-package org.proleesh.springbootinpractice3.repository;
-
-import org.proleesh.springbootinpractice3.entity.Course;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomizedCourseRepository extends BaseRepository<Course, Long>{
-}
--- SpringBootInPractice3/src/test/java/org/proleesh/springbootinpractice3/SpringBootInPractice3ApplicationTests.java
+++ SpringBootInPractice3/src/test/java/org/proleesh/springbootinpractice3/SpringBootInPractice3ApplicationTests.java
@@ -2,11 +2,14 @@ package org.proleesh.springbootinpractice3;
 
 import org.junit.jupiter.api.Test;
 import org.proleesh.springbootinpractice3.entity.Course;
-import org.proleesh.springbootinpractice3.repository.CustomizedCourseRepository;
+import org.proleesh.springbootinpractice3.repository.CourseRepository;
+//import org.proleesh.springbootinpractice3.repository.CustomizedCourseRepository;
+//import org.proleesh.springbootinpractice3.repository.CustomizedCourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,14 +17,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 class SpringBootInPractice3ApplicationTests {
 
     @Autowired
-    private CustomizedCourseRepository customizedCourseRepository;
+    private CourseRepository courseRepository;
 
     @Test
-    void contextLoads() {
-        Course course = new Course("Repid Spring Boot Application Development", "Sping",
-                4, "The description");
-        customizedCourseRepository.save(course);
-        assertThat(Arrays.asList(customizedCourseRepository.findAll()).size()).isEqualTo(1);
+    public void givenCreateCourseWhenLoadTheCourseThenExpectSameCourse(){
+        // 과정 목록 저장
+        courseRepository.saveAll(getCourseList());
+
+        // Spring 카테고리의 코스는 3개 있어야 함
+        assertThat(courseRepository.findAllByCategory("Spring")).hasSize(3);
+
+        // Java 과정은 존재하지 않음 (assert 수정)
+        assertThat(courseRepository.existsByName("Java")).isFalse();
+
+        // JavaScript 과정은 존재함
+        assertThat(courseRepository.existsByName("JavaScript")).isTrue();
+
+        // Python 카테고리 과정은 없으므로 0개여야 함 (assert 수정)
+        assertThat(courseRepository.countByCategory("Python")).isEqualTo(0);
+
+        // "Getting Start"로 시작하는 이름의 코스는 0개여야 함 (테스트 데이터에는 없으므로)
+        assertThat(courseRepository.findByNameStartsWith("Getting Start")).hasSize(0);
     }
 
+    private List<Course> getCourseList(){
+        Course course1 = new Course("Rapid Spring Boot Application Development", "Spring", 4, "Spring Boot gives all the power of the Spring Framework without all of the complexity");
+        Course course2 = new Course("Learn Spring Boot Application Development", "Spring", 5, "Spring Boot gives all the power of the Spring Framework without all of the complexity");
+        Course course3 = new Course("Master Spring Boot Application Development", "Spring", 3, "Spring Boot gives all the power of the Spring Framework without all of the complexity");
+        Course course4 = new Course("JavaScript", "JavaScript", 3, "JavaScript gives all the power of the Spring Framework without all of the complexity");
+        return Arrays.asList(course1, course2, course3, course4);
+    }
 }
