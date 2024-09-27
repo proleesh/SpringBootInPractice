9499014627f7249671668e3799688ef43c811ee1
proleesh <merci726@yahoo.com>
2024-09-27 10:02:34 +0900

    update about Spring Cloud vault

---
+++ SpringBootInPractice5/src/main/java/org/proleesh/SpringBootInPractice5Application.java
@@ -0,0 +1,13 @@
+package org.proleesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInPractice5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInPractice5Application.class, args);
+	}
+
+}
